3beea87acd792c17d5fec0f045e28dac7940b811
Abdullah Sezdi <abdullahsezeies@gmail.com>
2022-10-29 13:55:42 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { usePosition } from "use-position";
+import HavaDurumu from "./components/HavaDurumu";
+
+
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+
+function App() {
+
+  const [weather,setWeather]=useState()
+  const {latitude,longitude}=usePosition();
+
+  const getWeather= async(lat,lon) =>{
+    const key=process.env.REACT_APP_WEATHER_API_KEY;
+    try{
+      const {data}= await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`);
+      setWeather(data)
+    }catch{
+        alert("veri alırken bir problem yaşandı")
+    }
+    
+  };
+
+  useEffect(()=>{ 
+    latitude && longitude && getWeather(latitude,longitude)
+  },[latitude,longitude])
+
+
+
+
+
+
+  return (
+    <div className="App">
+    <HavaDurumu weather={weather}></HavaDurumu>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HavaDurumu.js
@@ -0,0 +1,8 @@
+const HavaDurumu = (props) => {
+    console.log(props.weather)
+    return <div> Hava durumu buraya
+        </div>
+
+}
+
+export default HavaDurumu;
